ba8d9ee26c50616f51171eb75965b029a32fe556
Aynna <aynavalentina@gmail.com>
2026-06-18 02:47:06 +0000

    atv1

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-let amigos=[]
+let amigos = [];
 
-function adicionarAmigos(){
-    let input = document.getElementById("NomeAmigo");
-    let nome = input.Value.trim();
+function adicionarAmigo() {
+    let campo = document.getElementById("nomeAmigo");
+    let nome = campo.value.trim();
 
-    if(nome===""){
-        alert("por favor,digite um nome");
+    if (nome === "") {
+        alert("Digite um nome!");
         return;
-
     }
 
     amigos.push(nome);
-    let lista=document.getElementById("lista de amigos");
-    lista.innerHTML="";
 
-    for(let i=0;i<amigos.length;i++){
-        lista.innerHTML+= <li> ${amigos[i]}</li>
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
     }
- campo.Value="";
 
- function sortearamigo(){
-     if(amigos.length=== 0){
-        alert("nenhum amigo adicionado!");
-         return;
-     }
-     let indice=Math.floor(Math.random()*amigos.length);
-     let sorteado=amigos[indice];
-     document.getElementById("resultado"),textConternt="Amigo Sorteado:"+sorteado;
+    campo.value = "";
+}
 
- }
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um amigo!");
+        return;
+    }
+
+    let indice = Math.floor(Math.random() * amigos.length);
+    let sorteado = amigos[indice];
+
+    document.getElementById("resultado").textContent =
+        "Amigo sorteado: " + sorteado;
 }
